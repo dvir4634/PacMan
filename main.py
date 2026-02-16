091c240dfd2071a1dca027bdc259403a5dbcdd7c
dvir4634 <dvir4634@gmail.com>
2026-02-16 17:27:16 +0200

    Logic Part

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Coin:
 class Character:
     def __init__(self,center_x,center_y,speed):
         self.center_x = center_x
-        self.center_y = center_y
+        self.center_y = center_y#
         self.speed = speed
         self.change_x = 0
         self.change_y = 0
